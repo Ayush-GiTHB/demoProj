8aa59924f5a78bf625f7b17567a78b8e9900fe8d
Ayush_Git <98519963+Ayush-GiTHB@users.noreply.github.com>
2024-11-30 17:27:32 +0530

    Update Main.java
    2nd commmit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("i am main");
+        System.out.println("i am main-2");
     }
 }
